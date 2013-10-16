5a898dbd852ac0f4b2f9c88563174cca4d1e3255
Christopher Cailleaux <Chris@MacBook-Air-de-Christopher.local>
2013-10-16 21:45:43 +0200

    initial commit

---
+++ application/views/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	   <meta charset="utf-8">
+	   <title>CloudNote</title>
+        <link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+                <a class="brand" href="#">CloudNote</a>
+                    <ul class="nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Notes</a></li>
+                        <li><a href="#">Reminders</a></li>
+                        <li><a href="#" class="">Sign in</a></li>
+                </ul>
+            </div>
+        </div>
---
+++ application/views/listNote.php
@@ -0,0 +1,15 @@
+<?php include "header.php"?>
+    <div class="container">
+        <table class="table table-striped">
+                    <tr>
+        <td>row 1, cell 1</td>
+        <td>row 1, cell 2</td>
+        </tr>
+        <tr>
+        <td>row 2, cell 1</td>
+        <td>row 2, cell 2</td>
+        </tr>
+        </table>
+    </div>
+
+<?php include "footer.php"?>
